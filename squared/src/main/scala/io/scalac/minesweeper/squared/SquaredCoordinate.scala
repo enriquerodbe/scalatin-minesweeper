d27df03c58263ce0e229c5d6074a6c998a9f19fe
Enrique Rodríguez <enrique.rodriguez@scalac.io>
2022-08-15 20:28:15 -0300

    Add simplest squared implementation that passes tests

---
+++ squared/src/main/scala/io/scalac/minesweeper/squared/SquaredCoordinate.scala
@@ -0,0 +1,7 @@
+package io.scalac.minesweeper.squared
+
+import io.scalac.minesweeper.api.Coordinate
+
+final case class SquaredCoordinate(x: Int, y: Int) extends Coordinate {
+  override val neighbors: Seq[Coordinate] = Seq.empty
+}
