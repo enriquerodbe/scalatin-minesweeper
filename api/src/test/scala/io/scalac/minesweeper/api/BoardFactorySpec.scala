7188f64f72b3be4c237ee89ef663afb604a31ef9
Enrique Rodríguez <enrique.rodriguez@scalac.io>
2022-08-16 16:43:52 -0300

    Add BoardFactorySpec to force improve implementation

---
+++ api/src/test/scala/io/scalac/minesweeper/api/BoardFactorySpec.scala
@@ -0,0 +1,17 @@
+package io.scalac.minesweeper.api
+
+import munit.FunSuite
+
+abstract class BoardFactorySpec(create: () => BoardFactory) extends FunSuite {
+  test("Should create board of given size") {
+    val board = create().create(5, _ => false)
+    assertEquals(board.allCoordinates.size, 5)
+  }
+
+  test("Should set mines correctly") {
+    val board = create().create(5, _.neighbors.size % 2 == 0)
+    board.allCoordinates.foreach(c =>
+      assertEquals(c.neighbors.size % 2 == 0, board.hasMine(c))
+    )
+  }
+}
